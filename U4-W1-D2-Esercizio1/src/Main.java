5f26830d3d07d092350b50f9d2d5c27996da7e6c
AleAngheben <144798020+AleAngheben@users.noreply.github.com>
2024-01-09 13:07:17 +0100

    ex 2 done

--- U4-W1-D2-Esercizio1/src/Main.java
+++ U4-W1-D2-Esercizio1/src/Main.java
@@ -10,14 +10,20 @@ System.out.println("");
         System.out.println(stringaPariDispari("ciao"));
         System.out.println("");
         System.out.println("seconda richiesta");
-
-System.out.println(annoBisestile(2023));
+        System.out.println(annoBisestile(2023));
         System.out.println("");
+
         System.out.println("Esercizio 2");
+        stampaInLettere(9);
+
         System.out.println("Esercizio 3");
         System.out.println("Esercizio 4");
     }
 
+
+    //ESERCIZIO 1
+
+    //PRIMA PARTE ES1
     public static boolean stringaPariDispari(String a){
 
         if(a.length() % 2 == 0 ){
@@ -26,13 +32,34 @@ System.out.println(annoBisestile(2023));
             return false;
         }
     }
-
+//SECONDA PARTE ES1
     public static boolean annoBisestile(int b){
         if( b%4==0 || b%100==0 && b% 400==0){
             return true;
         }
         else{
             return false;
+        }
+    }
+
+    //ESERCIZIO 2
+
+    public static void stampaInLettere (int n){
+
+        switch (n){
+            case 0:
+             System.out.println("Zero");
+            break;
+            case 1:
+                System.out.println("Uno");
+                break;
+            case 2:
+                System.out.println("Due");
+                break;
+            case 3:
+                System.out.println("Tre");
+                break;
+            default:System.out.println("Errore: numero non compreso tra 0 e 3");
         }
     }
 
